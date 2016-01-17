3a234cbfd8165b706988aa9adf608d25e2dc0c25
David Robb <daverobb1999@gmail.com>
2016-01-17 17:35:27 -0500

    1st push

--- bp-autocomplete/bp-autocomplete.php
+++ bp-autocomplete/bp-autocomplete.php
@@ -1,75 +1,85 @@
 <?php
+/*
+   Plugin Name: bp autocomplete
+   Plugin URI: http://wordpress.org/extend/plugins/bp-autocomplete/
+   Version: 0.1
+   Author: David Robb
+   Description: An autocomplete field for buddypress registration
+   Text Domain: bp-autocomplete
+   License: GPLv3
+  */
+
+/*
+    "WordPress Plugin Template" Copyright (C) 2016 Michael Simpson  (email : michael.d.simpson@gmail.com)
+
+    This following part of this file is part of WordPress Plugin Template for WordPress.
+
+    WordPress Plugin Template is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WordPress Plugin Template is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Contact Form to Database Extension.
+    If not, see http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+$BpAutocomplete_minimalRequiredPhpVersion = '5.0';
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              daverobbdesigns.com
- * @since             1.0.0
- * @package           Bp_Autocomplete
- *
- * @wordpress-plugin
- * Plugin Name:       bp-autocomplete
- * Plugin URI:        daverobbdesigns.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            David Robb
- * Author URI:        daverobbdesigns.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       bp-autocomplete
- * Domain Path:       /languages
+ * Check the PHP version and give a useful error message if the user's version is less than the required version
+ * @return boolean true if version check passed. If false, triggers an error which WP will handle, by displaying
+ * an error message on the Admin page
  */
-
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+function BpAutocomplete_noticePhpVersionWrong() {
+    global $BpAutocomplete_minimalRequiredPhpVersion;
+    echo '<div class="updated fade">' .
+      __('Error: plugin "bp autocomplete" requires a newer version of PHP to be running.',  'bp-autocomplete').
+            '<br/>' . __('Minimal version of PHP required: ', 'bp-autocomplete') . '<strong>' . $BpAutocomplete_minimalRequiredPhpVersion . '</strong>' .
+            '<br/>' . __('Your server\'s PHP version: ', 'bp-autocomplete') . '<strong>' . phpversion() . '</strong>' .
+         '</div>';
 }
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-bp-autocomplete-activator.php
- */
-function activate_bp_autocomplete() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-bp-autocomplete-activator.php';
-	Bp_Autocomplete_Activator::activate();
+
+function BpAutocomplete_PhpVersionCheck() {
+    global $BpAutocomplete_minimalRequiredPhpVersion;
+    if (version_compare(phpversion(), $BpAutocomplete_minimalRequiredPhpVersion) < 0) {
+        add_action('admin_notices', 'BpAutocomplete_noticePhpVersionWrong');
+        return false;
+    }
+    return true;
 }
 
+
 /**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-bp-autocomplete-deactivator.php
+ * Initialize internationalization (i18n) for this plugin.
+ * References:
+ *      http://codex.wordpress.org/I18n_for_WordPress_Developers
+ *      http://www.wdmac.com/how-to-create-a-po-language-translation#more-631
+ * @return void
  */
-function deactivate_bp_autocomplete() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-bp-autocomplete-deactivator.php';
-	Bp_Autocomplete_Deactivator::deactivate();
+function BpAutocomplete_i18n_init() {
+    $pluginDir = dirname(plugin_basename(__FILE__));
+    load_plugin_textdomain('bp-autocomplete', false, $pluginDir . '/languages/');
 }
 
-register_activation_hook( __FILE__, 'activate_bp_autocomplete' );
-register_deactivation_hook( __FILE__, 'deactivate_bp_autocomplete' );
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-bp-autocomplete.php';
+//////////////////////////////////
+// Run initialization
+/////////////////////////////////
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_bp_autocomplete() {
+// Initialize i18n
+add_action('plugins_loadedi','BpAutocomplete_i18n_init');
 
-	$plugin = new Bp_Autocomplete();
-	$plugin->run();
-
+// Run the version check.
+// If it is successful, continue with initialization for this plugin
+if (BpAutocomplete_PhpVersionCheck()) {
+    // Only load and run the init function if we know PHP version can parse it
+    include_once('bp-autocomplete_init.php');
+    BpAutocomplete_init(__FILE__);
 }
-run_bp_autocomplete();
